e309c8ea92dc34a487674ed4c0e2d6e823419b3e
Andrew Blair <andrew.blair@ucsf.edu>
2023-02-21 14:24:43 -0800

    Remove MPRAbase library

--- run-MPRAbase-JupyterLab.sh
+++ run-MPRAbase-JupyterLab.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-docker run -it --rm -p 10000:8888 -v "${PWD}":/home/jovyan/work apblair/mprabase:jupyterlab-dev-v0.1
+# "${PWD}"
+docker run -it --rm -p 10000:8888 -u root -v /Users/andrewblair/Desktop/Development:/home/jovyan/work apblair/mprabase:jupyterlab-dev-v0.1
