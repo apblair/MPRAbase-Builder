5c82868c878a8a718e6f2a77eba757d540012d06
Andrew Blair <andrew.blair@ucsf.edu>
2023-02-22 14:13:29 -0800

    Update example GSE83894

--- data/GSE83894.R
+++ data/GSE83894.R
@@ -2,5 +2,5 @@ library(MPRAbase)
 
 summary_df <- read.table("../inst/summary.csv", header=T, sep=',') # Load summary file
 geo_selection_df <- filter(summary_df, GEO_number=="GSE83894") # User select GEO identifier
-se <- ExportSummarizedExperiment(geo_selection_df) # Create the Summarized Experimen
+se <- create_summarized_experiment_object(geo_selection_df) # Create the Summarized Experiment
 
